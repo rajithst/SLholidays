9f623803e45d0905b183eaade8db9fc5332fc7b2
rajithst <rajiththennakoonshady@gmail.com>
2019-08-13 21:32:54 +0900

    add  refactoring to is holiday method

--- slholidays/slholidays.py
+++ slholidays/slholidays.py
@@ -11,9 +11,21 @@ holidays_dict.keys()
 
     
 def is_holiday(date):
-    inp =  datetime.datetime.strptime(date,"%Y-%m-%d")
-    date_s = datetime.datetime.strftime(inp,"%Y-%m-%d")
+    inp =  parse_date_to_obj(date)
+    date_s = parse_obj_to_string(inp)
     if date_s in holidays_dict.keys():
         return True
     else:
         return False
+    
+def get_previous_and_next_holiday(date,next_day=True,include_weekends=False):
+    pass
+    
+    
+    
+    
+def parse_obj_to_string(date):
+    return datetime.datetime.strftime(date,"%Y-%m-%d")
+
+def parse_date_to_obj(date):
+    return datetime.datetime.strptime(date,"%Y-%m-%d")
